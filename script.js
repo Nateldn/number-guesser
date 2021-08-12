1c091aa4935c1571fc5f463e69504713265a04a0
Nateldn <32700665+Nateldn@users.noreply.github.com>
2021-08-12 23:46:36 +0100

    Refactor targetNumber function

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Create targetNumber function, to return a random integer between 0 and 9.
 const generateTarget = () => {
-    let targetNumber = Math.floor(Math.random() * 9) + 1
-    return targetNumber
+    let targetNumber = Math.floor(Math.random() * 9) + 1;
+    return targetNumber;
+};
+
+
+// Determine which guess is closest to the target number
+
+const compareGuesses = (humanGuess, compGuess, secretTarget) => {
+
 };
