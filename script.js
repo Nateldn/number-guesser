470ca247b7e722b1a4926ee8dd4be59ebf150248
Nateldn <32700665+Nateldn@users.noreply.github.com>
2021-09-20 15:26:19 +0100

    Initial commit

--- script.js
+++ script.js
@@ -4,15 +4,38 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-// Create targetNumber function, to return a random integer between 0 and 9.
+// Returns a random integer between 0 and 9 used for the Computer's guess
 const generateTarget = () => {
     let targetNumber = Math.floor(Math.random() * 9) + 1;
     return targetNumber;
-};
+}
 
 
 // Determine which guess is closest to the target number
 
-const compareGuesses = (humanGuess, compGuess, secretTarget) => {
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+const humanDistance = Math.abs (secretTarget - humanGuess)
+const computerDistance = Math.abs (secretTarget - computerGuess)
+return humanDistance <= computerDistance;
 
-};
+}
+
+
+// Increases the winner’s score after each round
+
+const updateScore = winner => {
+    if (winner === 'human') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
+    }
+  }
+
+  // Increases the round number
+  const advanceRound = () => currentRoundNumber++;
+
+
+  updateScore('human');
+console.log(humanScore); // To confirm that this value increased by 1
+updateScore('computer');
+console.log(computerScore); // To confirm that this value increased by 1
